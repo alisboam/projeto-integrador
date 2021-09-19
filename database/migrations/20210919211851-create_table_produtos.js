1a1f1fb0120c7c04624b40a4f355b67cfb315651
Aline Lisboa <alisboam@gmail.com>
2021-09-19 18:27:05 -0300

    criando migration de produtos

---
+++ database/migrations/20210919211851-create_table_produtos.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    
+     await queryInterface.createTable('produtos', { 
+       
+      id: {
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+      type: Sequelize.BIGINT.UNSIGNED
+    },
+
+    preco: {
+      allowNull: false,
+      type: Sequelize.DOUBLE
+    },
+
+    descricao: {
+      allowNull: false,
+      type: Sequelize.STRING(1000)
+    },
+
+    categoria: {
+      allowNull: false,
+      type: Sequelize.STRING(100)
+    },
+
+    faixa_etaria: {
+      allowNull: false,
+      type: Sequelize.STRING(5)
+    },
+  },
+  {
+    tableName: "produtos",
+    timestamps: false
+  });
+},
+
+  down: async (queryInterface, Sequelize) => {
+    
+     await queryInterface.dropTable('produtos');
+  }
+};
