c1ab771a2af1212154c4e520463ff703e1740f37
Aline Lisboa <alisboam@gmail.com>
2021-11-07 16:04:21 -0300

    inserido busca de pedido

--- controllers/PedidoController.js
+++ controllers/PedidoController.js
@@ -3,3 +3,7 @@ const pedidoModel = require("../models/PedidoModel")
 exports.buscarPedidosUsuario = async (id) => {
   return await pedidoModel.buscarPedidosUsuario(id);  
 };
+
+exports.buscarPedido = async(idPedido, idUsuario) => {
+  return await pedidoModel.buscarPedido(idPedido, idUsuario);
+}
--- models/PedidoModel.js
+++ models/PedidoModel.js
@@ -1,6 +1,9 @@
-const { Pedido } = require("../database/models")
+const { Pedido } = require('../database/models')
 
+exports.buscarPedidosUsuario = async id => {
+  return Pedido.findAll({ where: { usuario_id: id } })
+}
 
-exports.buscarPedidosUsuario = async (id) => {
-  return Pedido.findAll({ where:{ usuario_id : id}})  
-};
+exports.buscarPedido = async (idPedido, idUsuario) => {
+  return Pedido.findAll({ where: { id: idPedido, usuario_id: idUsuario } })
+}
