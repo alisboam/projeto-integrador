902d220915e61e6329182972d59c9e3ec46881d6
Ana Paula <avasconcellospereira@hotmail.com>
2021-11-07 12:43:38 -0300

    Criação página entrega

--- controllers/EnderecoController.js
+++ controllers/EnderecoController.js
@@ -3,4 +3,8 @@ const UsuarioModel = require("../models/UsuarioModel")
 exports.cadastrarEndereco = async (address, idUsuario) => {
     console.log(address, "constroller")
     return await UsuarioModel.novoEndereco(address, idUsuario)
-}
+};
+
+exports.buscarEnderecoUsuario = async (idUsuario) => { 
+    return await UsuarioModel.buscarEnderecoUsuario(idUsuario)
+  };
